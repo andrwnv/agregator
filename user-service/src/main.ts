bdfe0cddd31a28ee36b3307e61cfaef8524bf3d7
Andrew G <glazynovand@gmail.com>
2021-10-17 00:43:31 +0700

    now user-service is server (nestjs/microservices deprecated)

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-        AppModule,
-        {
-            transport: Transport.TCP,
-            options: {
-                port: 3010
-            }
-        },
-    );
-
-    await app.listen();
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3010, () => {
+        console.log("User service listening on 3010");
+    });
 }
 
 bootstrap();
--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -1,19 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 import { IUser } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
 
-    @MessagePattern('get.user.list')
+    @Get()
     getAll() {
         return this.userService.getAll();
     }
 
-    @MessagePattern('create.user')
-    create(@Payload() msg: IUser) {
+    @Post()
+    create(@Body() msg: IUser) {
         return this.userService.createUser(msg);
     }
 }
