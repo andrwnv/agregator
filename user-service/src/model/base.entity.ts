82c04beb9dda8aa7c0e5fc26a0dd169a0b745785
Andrew G <glazynovand@gmail.com>
2021-10-22 18:02:41 +0700

    fix user base entity

--- user-service/src/model/base.entity.ts
+++ user-service/src/model/base.entity.ts
@@ -13,9 +13,6 @@ export abstract class BaseEntity {
     @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     createDateTime: Date;
 
-    @Column({ type: 'varchar', length: 300 })
-    createdBy: string;
-
     @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     lastChangedDateTime: Date;
 
