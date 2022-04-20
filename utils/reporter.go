ccccb6bada5f72aa3bb7d2ef56956d9ca70990cf
Andrew G <glazynovand@gmail.com>
2022-04-20 17:18:22 +0700

    fix reporter copypast

--- utils/reporter.go
+++ utils/reporter.go
@@ -7,13 +7,13 @@ func ReportCritical(msg string) {
 }
 
 func ReportError(msg string) {
-	panic(fmt.Sprintf("[ERR]: %s.", msg))
+	fmt.Println(fmt.Sprintf("[ERR]: %s.", msg))
 }
 
 func ReportWarning(msg string) {
-	panic(fmt.Sprintf("[WARN]: %s.", msg))
+	fmt.Println(fmt.Sprintf("[WARN]: %s.", msg))
 }
 
 func ReportInfo(msg string) {
-	panic(fmt.Sprintf("[INFO]: %s.", msg))
+	fmt.Println(fmt.Sprintf("[INFO]: %s.", msg))
 }
